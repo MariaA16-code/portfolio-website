ea70626bd49b054e81dc75e27a3f98c61c36d46a
MariaA16-code <70147167@student.uol.edu.pk>
2026-05-09 21:05:18 +0500

    feat: complete portfolio website

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// script.js
+
+// ACTIVE NAV LINK ON SCROLL
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 200;
+
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === `#${current}`){
+      link.classList.add("active");
+    }
+
+  });
+
+});
